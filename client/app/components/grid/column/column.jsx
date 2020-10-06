986cd5ea036b2d32e8bd2ed4535651d35e3c6280
Katia-2020 <kateflights@gmail.com>
2020-10-06 16:57:04 +0100

    grid/column/

---
+++ client/app/components/grid/column/column.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './column.scss';
+
+import classnames from 'classnames/bind'
+
+const cx = classnames.bind(styles);
+
+const Column = (props) => {
+  const {children, grow, shrink, center} = props;
+
+  return (
+    <div
+      className = {cx('grid__item',{
+        'grid__item--grow' : grow,
+        'grid__item--shrink': shrink,
+        'grid__item--center': center,
+
+      })}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Column;

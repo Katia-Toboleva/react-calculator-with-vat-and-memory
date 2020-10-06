26e2ca84aaf8b460563459eda3c1c98588a9d0a5
Katia-2020 <kateflights@gmail.com>
2020-10-06 16:15:27 +0100

    add client folder

---
+++ client/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Calculator } from './components/calculator'
+
+import './components/reset.scss'
+
+const App = () => {
+  return (
+  <Calculator />
+  )
+}
+
+export default App
+
+
